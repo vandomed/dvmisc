9f76831bbe30f6382cfaa34bd2af1d6c47cc8fb1
Dane Van Domelen <vandomed@gmail.com>
2018-09-27 14:49:06 -0400

    Added iterate function

---
+++ R/iterate.R
@@ -0,0 +1,44 @@
+#' Iterate Function Over All Combinations of User-Specified Inputs, Potentially 
+#' Multiple Times
+#' 
+#' Basically a wrapper for \code{\link[purrr:map2]{pmap}}, but with some 
+#' different functionality. It runs all combinations of inputs rather than the 
+#' 1st set, 2nd set, and so forth, and multiple trials can be run for each 
+#' scenario, which might be useful for running simulations.
+#' 
+#' @param f A function.
+#' @param ... Arguments to \code{f}, any of which can be vector-valued.
+#' @param trials Numeric value.
+#' 
+#' @return Data frame.
+#' 
+#' @examples
+#' # Define function to generate data from N(mu, sigsq) and perform t-test.
+#' f <- function(n = 100, mu = 0, sigsq = 1, alpha = 0.05) {
+#'   x <- rnorm(n = n, mean = mu, sd = sqrt(sigsq))
+#'   fit <- t.test(x = x, alpha = alpha)
+#'   return(list(t = fit$statistic, p = fit$p.value))
+#' }
+#' 
+#' # Call f once for various sample sizes and means
+#' f %>% iterate(n = c(100, 500), mu = c(0.1, 0.25))
+#' 
+#' # Run 100 trials for each scenario and calculate empirical power
+#' f %>% iterate(n = c(100, 500), mu = c(0.1, 0.25)) %>%
+#'   dplyr::group_by(n, mu) %>%
+#'   dplyr::summarise(mean(p < 0.05))
+#'
+#' @export
+iterate <- function(f, ..., trials = 1) {
+  
+  # Construct data frame with inputs to give to pmap
+  arg.combos <- expand.grid(list(...))
+  arg.combos <- arg.combos[rep(1: nrow(arg.combos), each = trials), ]
+  
+  # Use pmap to evaluate function for each combination of inputs
+  vals <- purrr::pmap(.l = arg.combos, .f = f)
+  
+  # Create and return data frame
+  return(cbind(arg.combos, do.call(rbind, vals)))
+  
+}
