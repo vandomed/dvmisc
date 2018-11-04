6ff2bfc1b15cbcaec98213b2935b12e0da8d5fee
Dane Van Domelen <vandomed@gmail.com>
2018-11-04 00:54:28 -0400

    Made expand_grid much faster

--- R/dvmisc.R
+++ R/dvmisc.R
@@ -9,7 +9,7 @@
 #' Package: \tab dvmisc \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.1.3 \cr
-#' Date: \tab 2018-11-02 \cr
+#' Date: \tab 2018-11-03 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #' 
--- R/expand_grid.R
+++ R/expand_grid.R
@@ -1,10 +1,9 @@
 #' Same as expand.grid but with Sequences Reversed
 #' 
-#' Loops over the last argument, then the second-last, and so on.
+#' Loops over the last argument, then the second-last, and so on. Should be 
+#' faster than \code{\link[base]{expand.grid}}.
 #' 
-#' @param ... See \code{\link[base]{expand.grid}}.
-#' @param KEEP.OUT.ATTRS See \code{\link[base]{expand.grid}}.
-#' @param stringsAsFactors See \code{\link[base]{expand.grid}}.
+#' @param ... Vectors you want all combinations of.
 #' 
 #' @return Data frame.
 #' 
@@ -14,8 +13,20 @@
 #' expand_grid(x = c("a", "b", "c"), y = c(1, 2), z = c(TRUE, FALSE))
 #' 
 #' @export
-expand_grid <- function(..., KEEP.OUT.ATTRS = TRUE, stringsAsFactors = TRUE) {
-  rev(expand.grid(rev(list(...)), 
-                  KEEP.OUT.ATTRS = KEEP.OUT.ATTRS, 
-                  stringsAsFactors = stringsAsFactors))
+expand_grid <- function(...) {
+  inputs.list <- list(...)
+  levels <- vapply(inputs.list, length, integer(1))
+  nrows <- prod(levels)
+  nreps <- nrows / cumprod(levels)
+  df <- mapply(
+    FUN = function(x, y, z) {
+      rep(rep(x, each = y), z)
+    },  
+    x = inputs.list, 
+    y = nreps, 
+    z = rev(nreps), 
+    SIMPLIFY = FALSE
+  )
+  setattr(df, "class", "data.table")
+  return(df)
 }
