c600e23edc64c678dcb5e49387b2dd88947d53dd
Dane Van Domelen <vandomed@gmail.com>
2019-05-09 11:25:57 -0400

    Added quantile_groups function

--- R/dvmisc.R
+++ R/dvmisc.R
@@ -9,7 +9,7 @@
 #' Package: \tab dvmisc \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.1.4 \cr
-#' Date: \tab 2019-04-05 \cr
+#' Date: \tab 2019-05-09 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #' 
---
+++ R/quantile_groups.R
@@ -0,0 +1,71 @@
+#' Create Quantile Groups
+#' 
+#' Combines \code{\link[stats]{quantile}} and \code{\link[base]{cut}} into a 
+#' single function, with strata-specific quantiles possible. For example, you 
+#' could create sex-specific height tertiles with 
+#' \code{quantile_groups(height, groups = 3, strata = sex)}. Compatible with 
+#' \strong{dplyr} functions like \code{\link[dplyr]{mutate}} and 
+#' \code{\link[dplyr:mutate]{transmute}}.
+#' 
+#' @param x Numeric vector.
+#' @param groups Numeric value, e.g. 3 for tertiles, 4 for quartiles, etc.
+#' @param strata Factor specifying subgroups to calculate quantiles within. For 
+#' multivariable subgroups, you can use \code{\link{interaction}}.
+#' @param quantile_list Arguments to pass to \code{\link[stats]{quantile}}.
+#' @param cut_list Arguments to pass to \code{\link{cut}}.
+#' 
+#' @return Factor variable.
+#' 
+#' @examples 
+#' # In mtcars dataset, create tertiles for mpg
+#' mtcars$mpg_tertiles <- quantile_groups(mtcars$mpg, groups = 3)
+#' table(mtcars$mpg_tertiles)
+#' 
+#' # Define tertile cutpoints separately for 4-, 6-, and 8-cylinder vehicles
+#' mtcars$mpg_tertiles <- quantile_groups(mtcars$mpg, groups = 3, strata = mtcars$cyl)
+#' table(mtcars$mpg_tertiles)
+#' 
+#' # Works with dplyr functions like mutate
+#' mtcars <- mtcars %>% 
+#'   dplyr::mutate(mpg_tertiles = quantile_groups(mpg, groups = 3, strata = cyl))
+#' table(mtcars$mpg_tertiles)
+#' 
+#' # Can embed in lm, glm, etc.
+#' summary(lm(mpg ~ quantile_groups(wt), data = mtcars))
+#' 
+#' @export
+# x <- mtcars$mpg
+# groups <- 4
+# strata <- as.factor(mtcars$cyl)
+# quantile_list <- list(na.rm = TRUE)
+# cut_list <- list(include.lowest = TRUE)
+# quantile_groups(mtcars$mpg)
+# quantile_groups(mtcars$mpg, strata = mtcars$cyl)
+# x <- rnorm(1000)
+# groups <- quantile_groups(x, 5)
+# table(groups)
+quantile_groups <- function(x, 
+                            groups = 4, 
+                            strata = NULL, 
+                            labels = NULL, 
+                            quantile_list = list(na.rm = TRUE), 
+                            cut_list = list(include.lowest = TRUE)) {
+  
+  if (is.null(strata)) {
+    cutpoints <- do.call(quantile, c(list(x = x, probs = seq(0, 1, 1 / groups)), quantile_list))
+    return(do.call(cut, c(list(x = x, breaks = cutpoints), cut_list)))
+  }
+  
+  if (class(strata) != "factor") strata <- as.factor(strata)
+  if (is.null(cut_list$labels)) cut_list$labels <- paste("Q", 1: groups, sep = "")
+  probs <- seq(0, 1, 1 / groups)
+  y <- rep(NA, length(x))
+  for (ii in levels(strata)) {
+    locs <- which(strata == ii)
+    x_ii <- x[locs]
+    cutpoints <- do.call(quantile, c(list(x = x_ii, probs = probs), quantile_list))
+    y[locs] <- as.vector(do.call(cut, c(list(x = x_ii, breaks = cutpoints), cut_list)))
+  }
+  as.factor(y)
+  
+}
